8f2cf3949dff0bd1f6a74da9c22537ebac4fd847
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-11-16 13:45:45 +0100

    [ADD] - basic files

---
+++ passengers/src/main/java/be/vinci/ipl/chattycar/passengers/models/Position.java
@@ -0,0 +1,17 @@
+package be.vinci.ipl.chattycar.passengers.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Position {
+  private double latitude;
+  private double longitude;
+}
